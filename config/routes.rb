ba125233c0433b12adf7f43f719cbf08ebc5ee92
codecademy <ccuser@codecademy.com>
2017-11-28 17:32:14 +0000

    Stories Form done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
   get '/about' => 'pages#about'
+  
+  
+  resources :signups, except: [:delete, :update, :show, :index]
+  
+  get '/signups/new' => 'signups#new'
+  get '/thanks' => 'signups#thanks'
+  post '/create' => 'signups#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
